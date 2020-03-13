0b2d26c62fa1bcb2df4762e22bccd3b089cf3223
celebrated-strategy-2405 <robertaledo22@gmail.com>
2020-03-13 01:55:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,7 @@ def line(katz_deli)
   line_positions = [];
   if katz_deli.empty?
   puts "The line is currently empty."
-end
-
+else
   counter = 1
   katz_deli.each do |customer|
   line_positions << "#{number}. #{customer}"
